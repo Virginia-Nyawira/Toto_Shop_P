5c0cb4dc5ca180ffc1f496410dbbda7cb98dbc67
VirginiaH <virginianyawira50@gmail.com>
2023-09-19 15:54:04 +0300

    ccc

--- lib/Presentation/home_view.dart
+++ lib/Presentation/home_view.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:toto_shop/Presentation/styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+  decoration: const BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage("assets/backg.jpg")
+    )
+  ),
+),
+Positioned(
+  top: 10,
+  child: AppBar(
+  backgroundColor: Colors.white54,
+  title: Text("Toto World",
+  style: titleStyle(),),
+)),
+// Positioned(child: GridView(gridDelegate: ))
+          
+        ],
+      ),
+    );
   }
 }
---
+++ lib/Presentation/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextStyle titleStyle(){
+  return TextStyle(
+    fontSize: 18.0,
+    color: Colors.purpleAccent,
+    letterSpacing: 2.0,
+  );
+}

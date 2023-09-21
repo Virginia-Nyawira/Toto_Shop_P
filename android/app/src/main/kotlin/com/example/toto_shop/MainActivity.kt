14bcac2d1907417ecb0f4853af3d8375ef3c6a06
VirginiaH <virginianyawira50@gmail.com>
2023-09-21 09:01:53 +0300

    C new

--- android/app/src/main/kotlin/com/example/toto_shop/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/toto_shop/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.toto_shop
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }

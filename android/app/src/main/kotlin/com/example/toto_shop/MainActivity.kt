e15e94097dc2e0b1c327f19551b2d91991434be3
VirginiaH <virginianyawira50@gmail.com>
2023-09-21 11:08:17 +0300

    splashScreen worked

--- android/app/src/main/kotlin/com/example/toto_shop/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/toto_shop/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.toto_shop
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
+import com.example.toto_shop.SplashView
 
 class MainActivity: FlutterActivity() {
     override fun provideSplashScreen(): SplashScreen? = SplashView()

4b74c2c3ab7443b1235e76e06dfdf09ed288bf24
VirginiaH <virginianyawira50@gmail.com>
2023-09-29 15:56:31 +0300

    bottom nav

--- lib/Widgets/bottom_nav_bar.dart
+++ lib/Widgets/bottom_nav_bar.dart
@@ -8,17 +8,44 @@ class BottomNavigation extends StatefulWidget {
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
-   int _currentIndex = 0;
+  int _currentIndex = 0;
   final List<Widget> _pages = [
-    Placeholder(),
-     Placeholder(),
-      Placeholder(),
-      //HomePage
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+    //HomePage
     // CartPage(),
     // AccountPage(),
   ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom Navigation Example'),
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
   }
 }

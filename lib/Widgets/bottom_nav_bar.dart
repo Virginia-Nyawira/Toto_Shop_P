c911fb745d865500aa7fb1e0717c33dc35103277
VirginiaH <virginianyawira50@gmail.com>
2023-09-29 16:18:43 +0300

    more on bnb

--- lib/Widgets/bottom_nav_bar.dart
+++ lib/Widgets/bottom_nav_bar.dart
@@ -21,11 +21,9 @@ class _BottomNavigationState extends State<BottomNavigation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bottom Navigation Example'),
-      ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 170, 211, 201),
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
